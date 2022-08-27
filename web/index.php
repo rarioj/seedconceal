7d4cfc12c55794e0fff6c9934c57bae5962d749b
Ario Jatmiko <rarioj@yahoo.co.id>
2022-08-28 07:51:35 +0800

    Removed jquery dependency. Set all forms autocomplete="off".

--- web/index.php
+++ web/index.php
@@ -31,30 +31,32 @@ $default_language = $sc->getConfig('default_language');
   <div class="sc-container sc-first">
     <div class="sc-inner">
       <div class="sc-heading">Generate</div>
-      <form id="sc-form-generate" class="sc-form" method="post" action="/generate.php">
-        <label for="label">
+      <form id="sc-form-generate" class="sc-form" method="post" action="/generate.php" autocomplete="off">
+        <label for="generate-label">
           <strong>Label</strong>
-          <input type="text" id="label" name="label">
+          <input type="text" id="generate-label" name="label">
         </label>
-        <label for="passphrase">
-          <strong>Passphrase</strong> - <small>Enter a passphrase to generate a deterministic wallet. Leave blank to generate a random wallet.</small>
-          <input type="text" id="passphrase" name="passphrase">
+        <label for="generate-passphrase">
+          <strong>Passphrase</strong>
+          <input type="text" id="generate-passphrase" name="passphrase">
+          <small>&bull; Enter a passphrase to generate a <strong>deterministic</strong> wallet or leave blank to generate a <strong>random</strong> wallet</small>
         </label>
-        <label for="salt" id="sc-label-salt">
+        <label for="generate-salt" id="label-generate-salt" style="display: none;">
           <strong>Salt</strong>
-          <input type="text" id="salt" name="salt" value="<?php echo $default_hash_salt; ?>">
+          <input type="text" id="generate-salt" name="salt" value="<?php echo $default_hash_salt; ?>">
         </label>
-        <label for="iteration" id="sc-label-iteration">
+        <label for="generate-iteration" id="label-generate-iteration" style="display: none;">
           <strong>Iteration</strong>
-          <input type="number" id="iteration" name="iteration" value="<?php echo $default_hash_iteration; ?>">
+          <input type="number" id="generate-iteration" name="iteration" value="<?php echo $default_hash_iteration; ?>">
         </label>
-        <label for="password" id="sc-label-password">
-          <strong>Password</strong> - <small>Optional but strongly recommended.</small>
-          <input type="password" id="password" name="password">
+        <label for="generate-password" id="label-generate-password" style="display: none;">
+          <strong>Password</strong>
+          <input type="password" id="generate-password" name="password">
+          <small>&bull; Optional but strongly recommended</small>
         </label>
-        <label for="lang">
+        <label for="generate-size">
           <strong>Byte size</strong>
-          <select id="size" name="size">
+          <select id="generate-size" name="size">
             <?php foreach ($sizes as $size_id => $size_label) { ?>
               <option value="<?php echo $size_id; ?>" <?php if ($size_id === $default_size) echo 'selected="selected"'; ?>><?php echo $size_label; ?> words</option>
             <?php } ?>
@@ -67,33 +69,35 @@ $default_language = $sc->getConfig('default_language');
   <div class="sc-container">
     <div class="sc-inner">
       <div class="sc-heading">Obscure</div>
-      <form id="sc-form-obscure" class="sc-form" method="post" action="/obscure.php">
-        <label for="label">
-          <strong>Label</strong> <input type="text" id="label" name="label">
+      <form id="sc-form-obscure" class="sc-form" method="post" action="/obscure.php" autocomplete="off">
+        <label for="obscure-label">
+          <strong>Label</strong>
+          <input type="text" id="obscure-label" name="label">
         </label>
-        <label for="mnemonic">
+        <label for="obscure-mnemonic">
           <strong>Seed phrase<sup>*</sup></strong>
-          <input type="text" id="mnemonic" name="mnemonic">
+          <input type="text" id="obscure-mnemonic" name="mnemonic">
         </label>
-        <label for="password">
-          <strong>Password</strong> - <small>Optional but strongly recommended.</small>
-          <input type="password" id="password" name="password">
+        <label for="obscure-password">
+          <strong>Password</strong>
+          <input type="password" id="obscure-password" name="password">
+          <small>&bull; Optional but strongly recommended</small>
         </label>
-        <label for="salt" id="sc-label-salt">
+        <label for="obscure-salt" id="label-obscure-salt" style="display: none;">
           <strong>Salt</strong>
-          <input type="text" id="salt" name="salt" value="<?php echo $default_hash_salt; ?>">
+          <input type="text" id="obscure-salt" name="salt" value="<?php echo $default_hash_salt; ?>">
         </label>
-        <label for="iteration" id="sc-label-iteration">
+        <label for="obscure-iteration" id="label-obscure-iteration" style="display: none;">
           <strong>Iteration</strong>
-          <input type="number" id="iteration" name="iteration" value="<?php echo $default_hash_iteration; ?>">
+          <input type="number" id="obscure-iteration" name="iteration" value="<?php echo $default_hash_iteration; ?>">
         </label>
-        <label for="split">
+        <label for="obscure-split">
           <strong>Split into</strong>
-          <input type="number" id="split" name="split" value="<?php echo $default_split; ?>">
+          <input type="number" id="obscure-split" name="split" value="<?php echo $default_split; ?>">
         </label>
-        <label for="lang">
+        <label for="obscure-lang">
           <strong>Language output</strong>
-          <select id="lang" name="lang">
+          <select id="obscure-lang" name="lang">
             <option value="<?php echo $random_language; ?>">Random</option>
             <?php foreach ($languages as $lang_id => $lang_label) { ?>
               <option value="<?php echo $lang_id; ?>" <?php if ($lang_id === $default_language) echo 'selected="selected"'; ?>><?php echo $lang_label; ?></option>
@@ -107,25 +111,27 @@ $default_language = $sc->getConfig('default_language');
   <div class="sc-container">
     <div class="sc-inner">
       <div class="sc-heading">Reveal</div>
-      <form id="sc-form-reveal" class="sc-form" method="post" action="/reveal.php">
-        <label for="label">
-          <strong>Label</strong> <input type="text" id="label" name="label">
+      <form id="sc-form-reveal" class="sc-form" method="post" action="/reveal.php" autocomplete="off">
+        <label for="reveal-label">
+          <strong>Label</strong>
+          <input type="text" id="reveal-label" name="label">
         </label>
-        <label for="mnemonic">
-          <strong>Seed phrase(s)<sup>*</sup></strong> - <small>Enter the seed phrases separated by newlines.</small>
-          <textarea id="mnemonic" name="mnemonic" rows="10"></textarea>
+        <label for="reveal-mnemonic">
+          <strong>Seed phrase(s)<sup>*</sup></strong>
+          <textarea id="mnemonic" name="reveal-mnemonic" rows="10"></textarea>
+          <small>&bull; Enter each seed phrase separated by newlines</small>
         </label>
-        <label for="password">
+        <label for="reveal-password">
           <strong>Password</strong>
-          <input type="password" id="password" name="password">
+          <input type="password" id="reveal-password" name="password">
         </label>
-        <label for="salt" id="sc-label-salt">
+        <label for="reveal-salt" id="label-reveal-salt" style="display: none;">
           <strong>Salt</strong>
-          <input type="text" id="salt" name="salt" value="<?php echo $default_hash_salt; ?>">
+          <input type="text" id="reveal-salt" name="salt" value="<?php echo $default_hash_salt; ?>">
         </label>
-        <label for="iteration" id="sc-label-iteration">
+        <label for="reveal-iteration" id="label-reveal-iteration" style="display: none;">
           <strong>Iteration</strong>
-          <input type="number" id="iteration" name="iteration" value="<?php echo $default_hash_iteration; ?>">
+          <input type="number" id="reveal-iteration" name="iteration" value="<?php echo $default_hash_iteration; ?>">
         </label>
         <p><button type="submit">Submit</button></p>
       </form>
@@ -134,39 +140,36 @@ $default_language = $sc->getConfig('default_language');
   <div class="sc-footer">
     <p><a href="https://github.com/rarioj/seedconceal">GitHub</a> &bull; Seed Conceal</p>
   </div>
-  <script type="text/javascript" src="/jquery.min.js"></script>
   <script type="text/javascript">
-    jQuery(document).ready(function($) {
-      $('#sc-form-generate #passphrase').on('keyup', function(e) {
-        if ($('#sc-form-generate #passphrase').val().trim().length < 1) {
-          $('#sc-form-generate #sc-label-salt').hide();
-          $('#sc-form-generate #sc-label-iteration').hide();
-          $('#sc-form-generate #sc-label-password').hide();
-        } else {
-          $('#sc-form-generate #sc-label-salt').show();
-          $('#sc-form-generate #sc-label-iteration').show();
-          $('#sc-form-generate #sc-label-password').show();
-        }
-      }).keyup();
-      $('#sc-form-obscure #password').on('keyup', function(e) {
-        if ($('#sc-form-obscure #password').val().trim().length < 1) {
-          $('#sc-form-obscure #sc-label-salt').hide();
-          $('#sc-form-obscure #sc-label-iteration').hide();
-        } else {
-          $('#sc-form-obscure #sc-label-salt').show();
-          $('#sc-form-obscure #sc-label-iteration').show();
-        }
-      }).keyup();
-      $('#sc-form-reveal #password').on('keyup', function(e) {
-        if ($('#sc-form-reveal #password').val().trim().length < 1) {
-          $('#sc-form-reveal #sc-label-salt').hide();
-          $('#sc-form-reveal #sc-label-iteration').hide();
-        } else {
-          $('#sc-form-reveal #sc-label-salt').show();
-          $('#sc-form-reveal #sc-label-iteration').show();
-        }
-      }).keyup();
-    });
+    document.getElementById('generate-passphrase').onkeyup = function() {
+      if (document.getElementById('generate-passphrase').value.trim().length < 1) {
+        document.getElementById('label-generate-salt').style.display = 'none';
+        document.getElementById('label-generate-iteration').style.display = 'none';
+        document.getElementById('label-generate-password').style.display = 'none';
+      } else {
+        document.getElementById('label-generate-salt').style.display = 'block';
+        document.getElementById('label-generate-iteration').style.display = 'block';
+        document.getElementById('label-generate-password').style.display = 'block';
+      }
+    };
+    document.getElementById('obscure-password').onkeyup = function() {
+      if (document.getElementById('obscure-password').value.trim().length < 1) {
+        document.getElementById('label-obscure-salt').style.display = 'none';
+        document.getElementById('label-obscure-iteration').style.display = 'none';
+      } else {
+        document.getElementById('label-obscure-salt').style.display = 'block';
+        document.getElementById('label-obscure-iteration').style.display = 'block';
+      }
+    };
+    document.getElementById('reveal-password').onkeyup = function() {
+      if (document.getElementById('reveal-password').value.trim().length < 1) {
+        document.getElementById('label-reveal-salt').style.display = 'none';
+        document.getElementById('label-reveal-iteration').style.display = 'none';
+      } else {
+        document.getElementById('label-reveal-salt').style.display = 'block';
+        document.getElementById('label-reveal-iteration').style.display = 'block';
+      }
+    };
   </script>
 </body>
 
